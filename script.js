2a296809a8bd94a223bf4f0439c92a0fa2312a47
Igor Eskin <eskinhd@gmail.com>
2019-02-06 17:35:50 -0500

    seed

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 
 
         $.getJSON("./students.json", function(json) {
-            console.log(json);
-            $(".container").text(JSON.stringify(json[0].firstname));
+            console.log(json.length);
+            $(".container").text(JSON.stringify(json));
         })
  
 
---
+++ seed.js
@@ -0,0 +1,43 @@
+[
+    {
+        "firstname" : "Stephan",
+        "lastname" : "Yu",
+        "email" : "stephan@itlize.com",
+        "location" : ["Piscataway", "New York", "California"],
+        "phone" : "2021231234",
+        "address" : {
+            "communication": "Itlize, Piscataway, New Jersey",
+            "permanent" : "China"
+        },
+        "marks" : {
+            "english" : "70",
+            "science" : "80",
+            "computers" : "90",
+            "hardware" : "80"
+        }
+    }
+]
+
+var obj = {
+    "firstname" : "Stephan",
+    "lastname" : "Yu",
+    "email" : "stephan@itlize.com",
+    "location" : ["Piscataway", "New York", "California"],
+    "phone" : "2021231234",
+    "address" : {
+        "communication": "Itlize, Piscataway, New Jersey",
+        "permanent" : "China"
+    },
+    "marks" : {
+        "english" : "70",
+        "science" : "80",
+        "computers" : "90",
+        "hardware" : "80"
+    }
+}
+
+// localStorage.setItem('students', obj);
+
+$.jStorage.set('key', 'test');  
+value = $.jStorage.get('key');
+alert("test"+value);
